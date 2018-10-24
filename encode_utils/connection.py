213f5c98bdcdfc1c242cd22b4dbbb53093f1b8e6
Nathan Watson <nathankw@Nathans-MacBook-Air.local>
2018-10-23 23:01:34 -0700

    dfjas

--- encode_utils/connection.py
+++ encode_utils/connection.py
@@ -838,6 +838,16 @@ class Connection():
 
         Before the POST is attempted, any pre-POST hooks are fist called (see the method
         ``self.before_submit_hooks``).
+
+        Args:
+            payload: `dict`. The data to submit.
+            require_aliases: `bool`.  `True` means that the 'aliases' property is to be required in
+                 `payload`. This is the default and it is highly recommended not to change this
+                 because it'll be easy to create duplicates on the server if accidentally POSTING
+                 the same payload again.  For example, you can easily create the same biosample
+                 as many times as you want on the Portal when not providing an alias.  Furthermore,
+                 submitting labs should include at least one alias per record being submitted
+                 to the Portal for traceabilty purposes in the submitting lab.
         """
         self.debug_logger.debug("\nIN post().")
         # Make sure we have a payload that can be converted to valid JSON, and
